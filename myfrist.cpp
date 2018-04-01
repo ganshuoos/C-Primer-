67619e37cbb2cce4df5a252328413c0710ab7463
ganshuoos <1804836778@qq.com>
2018-04-01 14:39:21 +0800

    2.1 myfrist.cpp 2018/4/1

---
+++ myfrist.cpp
@@ -0,0 +1,11 @@
+//myfrist.cpp -- displays a message
+
+#include<iostream>									//a PREPROCESSOR directive
+int main(int argc, char const *argv[])				//function header
+{	//start of functions visible
+	using namespace std;							//make definitions visible
+	cout << "Come up and C++ me some time.";		//message
+	cout << endl;									//start a new line
+	cout << "You won't regret it!" << endl;			//
+	return 0;										//terminate main()
+}													//end of function body
