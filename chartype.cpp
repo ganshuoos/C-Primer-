753ae8bda457338d1d7dea8ff7c44173f1db7765
ganshuoos <1804836778@qq.com>
2018-04-02 22:38:05 +0800

    3.5 chartype.cpp 2018/4/2

---
+++ chartype.cpp
@@ -0,0 +1,15 @@
+//chartype.cpp -- the char type
+
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+	using namespace std ;
+	char ch ;							//declare a char variable
+
+	cout << "Enter a character : " << endl;
+	cin >> ch ;
+	cout << "Holla! ";
+	cout << "Thank you for the " << ch << " character . " << endl;
+	return 0;
+}
