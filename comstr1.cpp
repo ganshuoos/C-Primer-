3c616988ed5d38196c72e95b44bb4bf359164344
ganshuoos <1804836778@qq.com>
2018-04-17 22:31:47 +0800

    5.11 comstr1.cpp 2018.04.17

---
+++ comstr1.cpp
@@ -0,0 +1,19 @@
+//comstr1.cpp -- comparing strings
+#include<iostream>
+#include<cstring>				//prototype for strcmp()
+int main(int argc, char const *argv[])
+{
+	/* code */
+	using namespace std;
+	char word[5] = "?ate";
+
+	for (char ch = 'a'; strcmp(word,"mate"); ch++)
+	{
+		/* code */
+		cout << word << endl;
+		word[0] = ch ;
+	}
+
+	cout << "After loop ends , word is " << word << endl;
+	return 0;
+}
