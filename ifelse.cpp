87e5c82d3d7d311063a4d2d1bd3d34c85a3012dc
ganshuoos <1804836778@qq.com>
2018-04-19 07:49:56 +0800

    6.2  ifelse.cpp 2018.04.19

---
+++ ifelse.cpp
@@ -0,0 +1,22 @@
+//ifelse.cpp -- using the if else statement
+#include<iostream>
+int main(int argc, char const *argv[])
+{
+	using namespace std;
+	char ch;
+
+	cout << "Type , and I shall repeat.\n";
+	cin.get(ch);
+	while(ch != '.'){
+		if (ch == '\n')
+		{
+			cout <<ch;
+		}
+		else
+		{
+			cout << ++ch ;
+		}
+		cin.get(ch);
+	}
+	return 0;
+}
