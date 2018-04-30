4a7a11dfd559457cd69cbc3fca6ace3825825310
ganshuoos <1804836778@qq.com>
2018-04-30 21:44:29 +0800

    8.3 secref.cpp 2018.04.30

---
+++ secref.cpp
@@ -0,0 +1,25 @@
+//secref.cpp -- defining and using a reference
+#include<iostream>
+int main(int argc, char const *argv[])
+{
+	using namespace std;
+	int rats = 101;
+	int & rodents = rats;
+
+	cout << "rats = " << rats;
+	cout << " , rodents = " << rodents << endl;
+
+	cout << "rats address = " << &rats;
+	cout << " , rodents address = " << &rodents << endl;
+
+	int bunnies = 50;
+	rodents = bunnies;
+	cout << "bunnies = " << bunnies;
+	cout << ", rats = " << rats;
+	cout << " , rodents = " << rodents << endl;
+	
+	cout << "rats address = " << &rats;
+	cout << " , rodents address = " << &rodents << endl;
+	cout << "bunnies address = " << &rats;
+	return 0;
+}
