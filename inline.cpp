221dd8fc74e4957f7f4a4cf8bcdd36586b96fc5d
ganshuoos <1804836778@qq.com>
2018-04-30 16:01:02 +0800

    8.1 inline.cpp 2018.04.30

---
+++ inline.cpp
@@ -0,0 +1,24 @@
+//using an inline function
+#include<iostream>
+
+inline double square(double x){
+	return x*x;
+}
+
+int main(int argc, char const *argv[])
+{
+	using namespace std;
+	double a,b;
+	double c = 13.0;
+
+	a = square (5.0);
+	b = square(4.5+7.5);
+
+	cout << "a = " << a << " , ";
+	cout << "b = " << b << endl;
+	cout << "c = " << c;
+	cout << " , c squared = " << square(c++) << endl;
+	cout << "Now c = " << c << endl;
+
+	return 0;
+}
